3a26443aef1df4d78ea7b13a891031fe0cc19fe2
Shemen E <128250146+shemeneroje@users.noreply.github.com>
2024-11-14 12:04:37 +0000

    Add files via upload

---
+++ ClimateApp.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package climateapp;
+
+/**
+ *
+ * @author shemeneroje
+ */
+public class ClimateApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        QuizGUI myGUI = new QuizGUI();
+        myGUI.setVisible(true);
+    }
+}
+   
